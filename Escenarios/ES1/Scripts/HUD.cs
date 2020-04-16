bf61cbf6905d6a68a19db9ec3c930ee304db7c50
olefran <oscar_lefran@hotmail.comp>
2020-04-15 19:16:55 -0500

    Implementar el boton de reseteo dentro del juego

--- Escenarios/ES1/Scripts/HUD.cs
+++ Escenarios/ES1/Scripts/HUD.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HUD : MonoBehaviour
 {
@@ -11,14 +12,26 @@ public class HUD : MonoBehaviour
 
     void Start()
     {
-        
+        Score.text = "0";
     }
 
     void Update()
     {
-        
+        if (GlobalVariables.lives > 0) {
+            LivesUI.sprite = LivesSprites[GlobalVariables.lives];
+        } else {
+            LivesUI.sprite = LivesSprites[0];
+        }
 
         Score.text = GlobalVariables.score.ToString();
         
     }
+
+    public void ResetGame() //TODO Beutify the reset button with a warning
+    {
+        GlobalVariables.lives = 5;
+        GlobalVariables.score = 0;
+        SceneManager.LoadScene("P1"); //TODO : Replace for Main Menu
+    }
+
 }
