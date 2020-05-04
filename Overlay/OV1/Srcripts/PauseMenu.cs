143007316cf6796ef5b2d26dddefa191c674caf8
Ricardo Reyes <alcala_ricardo@hotmail.com>
2020-05-04 13:31:54 -0500

    La creacion del Folder Overlay, El Menu principal y el menu de pausa (#14)

---
+++ Overlay/OV1/Srcripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Variables publicas
+    public Button Continuar;
+    public Button Salir;
+    GameObject MainCa ;
+    GameMind Script;
+
+    private void Start()
+    {
+        MainCa = GameObject.Find("Main Camera");
+        //Script = GameObject.Find("Main Camera").GetComponent<GameMind>.();
+    }
+
+    private void OnEnable()
+    {
+        Continuar.onClick.AddListener(delegate
+        {
+            MainCa.GetComponent<GameMind>().Pausar();
+        });
+
+        Salir.onClick.AddListener(delegate
+        {
+            SceneManager.LoadScene("Menu");
+        });
+
+    }
+}
