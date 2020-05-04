d30bec94173d0c66f607c3c5bb090d1384d96363
Ricardo Reyes <alcala_ricardo@hotmail.com>
2020-05-04 14:24:13 -0500

    Rx159/user story es2/jugar escenario2 (#15)
    * La creacion del Folder Overlay, El Menu principal y el menu de pausa
    
    * Poder jugar el Escenario 2 y optimizaciones al codigo

--- Escenarios/ES1/Scripts/BtnMangment.cs
+++ Escenarios/ES1/Scripts/BtnMangment.cs
@@ -9,8 +9,22 @@ public class BtnMangment : MonoBehaviour
 {
     // Variables publicas
     public Button Boton;
-    public int SigEscena;
+    public string SigEscena;
     public Text DialogueText;
+    GameObject[] ListaDItems;
+    float[] PosX = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    float[] PosY = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int cuantosItems;
+    string Item;
+    string ItemBox;
+    string ItemSlot;
+
+    private void Awake()
+    {
+        ContarItems();
+        FindItems();
+        Shuffle();
+    }
 
     private void OnEnable()
     {
@@ -29,6 +43,7 @@ public class BtnMangment : MonoBehaviour
                     // Quita vida y suma puntos
                     GameMind.takeAwayLive(1);
                     GameMind.addPoints(-100);
+                    Solution();
                     StartCoroutine(WaitSeconds(5));
                 }
             }
@@ -45,6 +60,7 @@ public class BtnMangment : MonoBehaviour
                     // Quita vida y suma puntos
                     GameMind.takeAwayLive(1);
                     GameMind.addPoints(-100);
+                    Solution();
                     StartCoroutine(WaitSeconds(5));
                 }
                 else if (DragDrops.statusAnswer() == "Incorrect" && GlobalVariables.pairAnswerSlot.Count == 7) {
@@ -52,6 +68,7 @@ public class BtnMangment : MonoBehaviour
                     // Quita vida y suma puntos
                     GameMind.takeAwayLive(1);
                     GameMind.addPoints(-100);
+                    Solution();
                     StartCoroutine(WaitSeconds(5));
                 }
                 else if (DragDrops.statusAnswer() == "Incorrect" && GlobalVariables.pairAnswerSlot.Count != 7) {
@@ -59,8 +76,89 @@ public class BtnMangment : MonoBehaviour
                     // Quita vida y suma puntos
                     GameMind.takeAwayLive(1);
                     GameMind.addPoints(-100);
+                    Solution();
                     StartCoroutine(WaitSeconds(5));
                 }
+
+            }
+            // Si la escena en juego es la ES2P3
+            if (SceneManager.GetActiveScene().name == "ES2P3")
+            {
+                if (DragDrops.statusAnswer() == "Correct" && GlobalVariables.pairAnswerSlot.Count == 1)
+                {
+                    DialogueText.text = "Correcto! Usaste tus sentidos de manera correcta";
+                    // Suma puntos
+                    GameMind.addPoints(100);
+                    StartCoroutine(WaitSeconds(5));
+                }
+                else if (DragDrops.statusAnswer() == "Correct" && GlobalVariables.pairAnswerSlot.Count != 1)
+                {
+                    DialogueText.text = "Incorrecto! Te faltaron pasos, lo correcto sería ...";
+                    Debug.Log(GlobalVariables.pairAnswerSlot.Count);
+                    // Quita vida y suma puntos
+                    GameMind.takeAwayLive(1);
+                    GameMind.addPoints(-100);
+                    Solution();
+                    StartCoroutine(WaitSeconds(5));
+                }
+                else if (DragDrops.statusAnswer() == "Incorrect" && GlobalVariables.pairAnswerSlot.Count == 1)
+                {
+                    DialogueText.text = "Incorrecto! lo correcto sería ...";
+                    // Quita vida y suma puntos
+                    GameMind.takeAwayLive(1);
+                    GameMind.addPoints(-100);
+                    Solution();
+                    StartCoroutine(WaitSeconds(5));
+                }
+                else if (DragDrops.statusAnswer() == "Incorrect" && GlobalVariables.pairAnswerSlot.Count != 1)
+                {
+                    DialogueText.text = "Incorrecto! Te faltaron pasos y lo correcto sería ...";
+                    // Quita vida y suma puntos
+                    GameMind.takeAwayLive(1);
+                    GameMind.addPoints(-100);
+                    Solution();
+                    StartCoroutine(WaitSeconds(5));
+                }
+
+            }
+            // Si la escena en juego es la ES2P5
+            if (SceneManager.GetActiveScene().name == "ES2P5")
+            {
+                if (DragDrops.statusAnswer() == "Correct" && GlobalVariables.pairAnswerSlot.Count == 3)
+                {
+                    DialogueText.text = "Correcto! Seguiste el orden optimo para el bloqueo";
+                    // Suma puntos
+                    GameMind.addPoints(100);
+                    StartCoroutine(WaitSeconds(5));
+                }
+                else if (DragDrops.statusAnswer() == "Correct" && GlobalVariables.pairAnswerSlot.Count != 3)
+                {
+                    DialogueText.text = "Incorrecto! Te faltaron pasos, lo correcto sería ...";
+                    // Quita vida y suma puntos
+                    GameMind.takeAwayLive(1);
+                    GameMind.addPoints(-100);
+                    Solution();
+                    StartCoroutine(WaitSeconds(5));
+                }
+                else if (DragDrops.statusAnswer() == "Incorrect" && GlobalVariables.pairAnswerSlot.Count == 3)
+                {
+                    DialogueText.text = "Incorrecto! el orden correcto sería ...";
+                    // Quita vida y suma puntos
+                    GameMind.takeAwayLive(1);
+                    GameMind.addPoints(-100);
+                    Solution();
+                    StartCoroutine(WaitSeconds(5));
+                }
+                else if (DragDrops.statusAnswer() == "Incorrect" && GlobalVariables.pairAnswerSlot.Count != 3)
+                {
+                    DialogueText.text = "Incorrecto! Te faltaron pasos y el orden correcto sería ...";
+                    // Quita vida y suma puntos
+                    GameMind.takeAwayLive(1);
+                    GameMind.addPoints(-100);
+                    Solution();
+                    StartCoroutine(WaitSeconds(5));
+                }
+
             }
         });
     }
@@ -76,4 +174,100 @@ public class BtnMangment : MonoBehaviour
             SceneManager.LoadScene(SigEscena);
         }
     }
+
+    //Ricky
+    //Cambiar dew posicion las cosas
+    public void Shuffle()
+    {
+        int topRange = cuantosItems + 1;
+        int Rand = Random.Range(1, topRange);
+        for (int i = 1; i <= cuantosItems; i++)
+        {
+            while (PosX[Rand] == 0)
+            {
+                Rand = Random.Range(1, topRange);
+            }
+            ItemBox = "ItemBox" + i;
+            GameObject Dummy = GameObject.Find(ItemBox);
+            Dummy.transform.localPosition = new Vector2(PosX[Rand], PosY[Rand]);
+            PosX[Rand] = 0;
+            PosY[Rand] = 0;
+        }
+    }
+
+    public void FindItems()
+    { 
+        for (int i = 1; i <= cuantosItems; i++)
+        {
+            ItemBox = "ItemBox" + i;
+            GameObject Dummy = GameObject.Find(ItemBox) ;
+            PosX[i] = Dummy.transform.localPosition.x;
+            PosY[i] = Dummy.transform.localPosition.y;
+        }
+    }
+
+    public void Solution()
+    {
+        string Escena = SceneManager.GetActiveScene().name;
+
+        if (Escena == "P2")
+        {
+            GameObject Slot = GameObject.Find("ItemSlot");
+            //Lentes
+            GameObject Item1 = GameObject.Find("Item1");
+            //Casco
+            GameObject Item2 = GameObject.Find("Item2");
+            //Boots
+            GameObject Item3 = GameObject.Find("Item3");
+            //Guantes
+            GameObject Item4 = GameObject.Find("Item4");
+
+            Item1.transform.localPosition = new Vector2(Slot.transform.localPosition.x, 94);
+            Item2.transform.localPosition = new Vector2(Slot.transform.localPosition.x, 214);
+            Item3.transform.localPosition = new Vector2(Slot.transform.localPosition.x, -146);
+            Item4.transform.localPosition = new Vector2(Slot.transform.localPosition.x, -26);
+
+        }
+        else
+        {
+            for (int i = 1; i <= cuantosItems; i++)
+            {
+                //La cosa
+                ItemBox = "ItemBox" + i;
+                // Donde va
+                ItemSlot = "ItemSlot" + i;
+
+                GameObject Box = GameObject.Find(ItemBox);
+                GameObject Slot = GameObject.Find(ItemSlot);
+
+                if (Slot != null)
+                {
+                    Box.transform.localPosition = new Vector2(Slot.transform.localPosition.x, Slot.transform.localPosition.y);
+                }
+                else
+                {
+                    Box.transform.localPosition = new Vector2(-1000, -1000);
+                }
+            }
+        }
+    }
+
+    public void ContarItems()
+    {
+        int i = 1;
+
+        ItemBox = "ItemBox" + i;
+        GameObject Box = GameObject.Find(ItemBox);
+
+        while (Box != null)
+        {
+            i++;
+            ItemBox = "ItemBox" + i;
+            Box = GameObject.Find(ItemBox);
+        }
+        cuantosItems = i-1;
+        Debug.Log(cuantosItems);
+    }
 }
+
+
--- Escenarios/ES1/Scripts/WinCase.cs
+++ Escenarios/ES1/Scripts/WinCase.cs
@@ -20,6 +20,7 @@ public class WinCase : MonoBehaviour {
         }
         
         textOption = StarNumber(GlobalVariables.score);
+
         if (SceneManager.GetActiveScene().name == "Win"){
             if (textOption == 0) {
                 Title.text = "Completaste la misión pero tu desempeño fue pésimo. Guardia, es tiempo de volver al entrenamiento";
@@ -32,9 +33,10 @@ public class WinCase : MonoBehaviour {
             } else if (textOption == 4) {
                 Title.text = "Muy cerca de la perfección, busca apoyo de un supervisor para mejorar";
             } else if (textOption == 5) {
-                Title.text = "¡Muy bien! ¡Ahora saber cómo reparar un rodillo!";
+                Title.text = "¡Muy bien! ¡Ahora sabes cómo reparar un rodillo!";
             }
-        } else if (SceneManager.GetActiveScene().name == "Lose") {
+        }
+        else if (SceneManager.GetActiveScene().name == "Lose") {
             if (textOption == 0) {
                 Title.text = "Perdiste, y tuviste un pésimo desempeño. Guardia, es tiempo de volver al entrenamiento";
             } else if (textOption == 1) {
@@ -49,7 +51,63 @@ public class WinCase : MonoBehaviour {
                 Title.text = "";
             }
         }
-        
+
+        //ES2
+        if (SceneManager.GetActiveScene().name == "ES2Win")
+        {
+            if (textOption == 0)
+            {
+                Title.text = "Completaste la misión pero tu desempeño fue pésimo. Guardia, es tiempo de volver al entrenamiento";
+            }
+            else if (textOption == 1)
+            {
+                Title.text = "Completaste la misión pero tu desempeño fue muy malo. A este paso si no le dedicas 100% a aprender podrias perder el rango de guardia ";
+            }
+            else if (textOption == 2)
+            {
+                Title.text = "Completaste la misión pero hubo mal desempeño, enfocate mucho en los manuales y busca alguien que te supervise";
+            }
+            else if (textOption == 3)
+            {
+                Title.text = "Desempeño promedio, esfuerzate un poco más en los manuales y trata de perfeccionar la mision";
+            }
+            else if (textOption == 4)
+            {
+                Title.text = "Muy cerca de la perfección, busca apoyo de un supervisor para mejorar";
+            }
+            else if (textOption == 5)
+            {
+                Title.text = "¡Muy bien! ¡Ahora sabes como actuar cuando escuches una averia!";
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "ES2Lose")
+        {
+            if (textOption == 0)
+            {
+                Title.text = "Perdiste, y tuviste un pésimo desempeño. Guardia, es tiempo de volver al entrenamiento";
+            }
+            else if (textOption == 1)
+            {
+                Title.text = "Perdiste y tu desempeño fue muy malo. A este paso si no le dedicas 100% a aprender podrias perder el rango de guardia ";
+            }
+            else if (textOption == 2)
+            {
+                Title.text = "Perdiste hubo un mal desempeño, enfocate mucho en los manuales para ganar la misión";
+            }
+            else if (textOption == 3)
+            {
+                Title.text = "Perdiste y tu desempeño fue promedio, esfuerzate un poco más en los manuales y trata de ganar la misión";
+            }
+            else if (textOption == 4)
+            {
+                Title.text = "Perdiste pero estás cerca de la perfección, busca apoyo de un supervisor para mejorar";
+            }
+            else if (textOption == 5)
+            {
+                Title.text = "";
+            }
+        }
+
 
         Stars1UI.sprite = StarSprites[textOption];
         Stars2UI.sprite = StarSprites[StarNumber(PlayerPrefs.GetInt("Highscore", GlobalVariables.score))];
@@ -57,20 +115,57 @@ public class WinCase : MonoBehaviour {
 
     // Funcion que regresa la cantidad de estrellas necesaria
     public int StarNumber(int points) {
+
         int n = 0;
+        string Escena = SceneManager.GetActiveScene().name;
+        string Escenario;
+        int Maximo;
+
+        if (Escena.Substring(0,1) == "W" || Escena.Substring(0, 1) == "L")
+        {
+            Escenario = "1";
+        }
+        else
+        {
+            Escenario = Escena.Substring(2, 1);
+        }
+
+        switch (Escenario)
+        {
+            //ES1
+            case "1": Maximo = 800; break;
+            case "2": Maximo = 900; break;
+            case "3": Maximo = 1000000; break;
+            case "4": Maximo = 1000000; break;
+            case "5": Maximo = 1000000; break;
+            case "6": Maximo = 1000000; break;
+            case "7": Maximo = 1000000; break;
+            case "8": Maximo = 1000000; break;
+            case "9": Maximo = 1000000; break;
+            case "10": Maximo = 1000000; break;
+
+            default: Maximo = 1000000; break;
+        }
+
+        points = points / Maximo;
+
+        //Debug.Log(Maximo);
+        //Debug.Log(points);
+
         if (points <= 0) {
             n = 0;
-        } else if (0 < points && points <= 200) {
+        } else if (0 < points && points <= .2) {
             n = 1;
-        } else if (200 < points && points <= 400) {
+        } else if (.2 < points && points <= .4) {
             n = 2;
-        }  else if (400 < points && points <= 600) {
+        }  else if (.4 < points && points <= .6) {
             n = 3;
-        }  else if (600 < points && points <= 700) {
+        }  else if (.6 < points && points <= .8) {
             n = 4;
-        }  else if (700 < points && points <= 800) {
+        }  else if (.8 < points && points <= 1) {
             n = 5;
         }
+
         return n;
     } 
 
