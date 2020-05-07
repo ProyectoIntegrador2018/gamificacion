94b6490b1e0457fa38858d0ae3c0e9782c5f268c
fabianaserangelli <fabiana.serangelli@hotmail.com>
2020-05-06 21:01:11 -0500

    Tutorial que aparece cuando no ha jugado ninguna mision

---
+++ Overlay/OV2/Scripts/InstructionDragDrop.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
+using System.IO;
+
+// Clase para los objetos que son arrastrables
+public class InstructionDragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler {
+
+    // Variables privadas
+    [SerializeField] private Canvas canvas = null;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private Vector3 defaultPos;
+    private bool itemWasHere = false;
+
+    // Variables publicas
+    public bool droppedOnSlot = false;
+
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        defaultPos = GetComponent<RectTransform>().localPosition;
+    }
+
+    // Objeto se empieza a arrastrar
+    public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+        droppedOnSlot = false;
+    }
+
+    // Objeto siendo arrastrado
+    public void OnDrag(PointerEventData eventData) {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    // Objeto dejo de ser arrastrado
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+        if (droppedOnSlot == false && itemWasHere == true) {
+            Debug.Log("Out of area");
+            rectTransform.anchoredPosition = defaultPos;  
+            if (InstructionGV.items.Count > 0) {
+                InstructionGV.items.Remove(this.gameObject);
+                Debug.Log("Removed");
+            }
+        }
+        if (droppedOnSlot == false && itemWasHere == false) {
+            Debug.Log("Out of area");
+            rectTransform.anchoredPosition = defaultPos;  
+        }
+        if (droppedOnSlot == true) {
+            Debug.Log("Inside of area");
+            InstructionGV.items.Add(this.gameObject);
+            Debug.Log("Added");
+            itemWasHere = true;
+        }
+    }
+
+    // Mouse presionado
+    public void OnPointerDown(PointerEventData eventData) {
+        Debug.Log("OnPointerDown");
+    }
+
+    // Estado que evalua si los objetos dentro del item slot son los correctos
+    public static string statusAnswer() {
+        Debug.Log(InstructionGV.items.Count);
+        if (InstructionGV.items.Exists(x => x.name == "Item1") && 
+            InstructionGV.items.Exists(x => x.name == "Item2") && 
+            InstructionGV.items.Exists(x => x.name == "Item3") && 
+            InstructionGV.items.Exists(x => x.name == "Item4") && 
+            InstructionGV.items.Count == 4) {
+            Debug.Log("Correct");
+            return "Correct";
+        } else {
+            Debug.Log("Incorrect");
+            return "Incorrect";
+        }
+    }
+}
+
---
+++ Overlay/OV2/Scripts/InstructionItemSlot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+// Clase para el espacio donde se deposita un objeto
+public class InstructionItemSlot : MonoBehaviour, IDropHandler {
+
+    // Funcion donde identifica cuando se deja caer un objto en el espacio
+    public void OnDrop(PointerEventData eventData) {
+    	eventData.pointerDrag.GetComponent<InstructionDragDrop>().droppedOnSlot = true;
+        Debug.Log("OnDrop");
+        GameObject droppedObject = eventData.pointerDrag;
+        if (eventData.pointerDrag != null) {
+        	Vector3 position = GetComponent<RectTransform>().anchoredPosition;
+        	position.y += InstructionGV.sumPos;
+            droppedObject.GetComponent<RectTransform>().anchoredPosition = position;
+            InstructionGV.sumPos = InstructionGV.sumPos - 90;
+        }
+    }
+}
