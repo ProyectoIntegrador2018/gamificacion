07d8e4fe33f98e507e04859b34d0a6a5b31fb4d9
olefran <oscar_lefran@hotmail.comp>
2020-05-25 22:40:16 -0500

    Historial de Casos y Estructura de Caso 6

---
+++ Escenarios/OV4/Scripts/MisionFunction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MisionFunction : MonoBehaviour
+{
+    public static string Description = "Aqui aparecerá la información de una mision selecionada.";
+    public static string Score = "0";
+    public static int Escenario = 1;
+    public GameObject self;
+    //Function transported from AchivementManager in order to be dinamically called.
+    public void changeInfo()
+    {
+        Description = self.transform.GetChild(2).GetComponent<Text>().text;
+        Score = self.transform.GetChild(3).GetComponent<Text>().text;
+        Escenario = int.Parse(self.transform.GetChild(4).GetComponent<Text>().text);
+    }
+
+}
