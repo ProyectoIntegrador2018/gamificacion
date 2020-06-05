04d6f25e18a1597214e8ef191954c130cdc52f57
Ricardo Reyes <alcala_ricardo@hotmail.com>
2020-06-05 16:45:57 -0500

    En teoria lo arregle

---
+++ Escenarios/ES1/Scripts/AchivementFunction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchivementFunction : MonoBehaviour
+{
+    public static string Description = "Se muestra aquí la descripción del un trofeo seleccionado, \n ¡Adelante, presiona uno!";
+    public GameObject self;
+    //Function transported from AchivementManager in order to be dinamically called.
+    public void changeDescription()
+    {
+        //AchievementManager.SetDescription( self.transform.GetChild(2).GetComponent<Text>().text );
+        Description = self.transform.GetChild(2).GetComponent<Text>().text;
+    }
+}
